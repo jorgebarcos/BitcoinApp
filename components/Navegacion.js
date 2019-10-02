08465c071286e1156e24fcb51c4a07c776c18c59
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-02 19:47:24 -0300

    Creando una barra de Navegación

--- components/Navegacion.js
+++ components/Navegacion.js
@@ -1,42 +1,27 @@
 import Link from 'next/link';
 
 const Navegacion = () => (
-	<div>
-		<ul>
-			<h1>Nombre del Sitio</h1>
-			<li>
-				<Link href="/">
-					<a>Inicio</a>
-				</Link>
-			</li>
-			<li>
-				<Link href="/nosotros">
-					<a>Nosotros</a>
-				</Link>
-			</li>
-		</ul>
-		<style jsx>
-			{`
-				h1 {
-					color: red;
-				}
-				ul {
-					background-color: #333;
-					list-style: none;
-					display: flex;
-				}
-				ul li {
-					paddin: .5rem 0;
-					margin-right: 1rem;
-				}
-				ul li a {
-					font-size: 1.2rem;
-					color: white;
-					text-decoration: none;
-				}
-			`}
-		</style>
-	</div>
+	<nav className="navbar navbar-expand navbar-dark bg-warning">
+		<div className="container">
+			<Link href="/">
+				<a className="navbar-brand">TodoBitcoin</a>
+			</Link>
+			<div className="collapse navbar-collapse">
+				<ul className="navbar-nav ml-auto">
+					<li className="nav-item">
+						<Link href="/">
+							<a className="nav-link">Inicio</a>
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link href="/nosotros">
+							<a className="nav-link">Nosotros</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 );
 
 export default Navegacion;
