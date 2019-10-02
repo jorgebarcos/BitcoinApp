d14df042d135c148ff854c18bf45fbbbf6b63935
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-02 20:01:45 -0300

    Creando un llamado a una API con Fetch

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import MasterPage from '../components/Master';
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => (
+const Index = (props) => (
 	<MasterPage>
 		<div className="row">
 			<div className="col-12">
 				<h2>Precio del Bitcoin</h2>
+				{console.log(props)}
 			</div>
 
 			<div className="col-md-8">
@@ -16,5 +18,15 @@ const Index = () => (
 		</div>
 	</MasterPage>
 );
+
+Index.getInitialProps = async () => {
+	const precio = await fetch('https://api.coinmarketcap.com/v2/ticker/1/');
+
+	const resPrecio = await precio.json();
+
+	return {
+		precioBitcoin: resPrecio.data.quotes.USD
+	};
+};
 
 export default Index;
